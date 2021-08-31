3465cc6c53bef2ba2c319ec4552a17cda3a8a6c2
Giuseppe Russo <giuseppe.russo.dev@gmail.com>
2021-08-31 18:01:53 +0200

    Add first part of browse page

---
+++ src/views/Browse.vue
@@ -0,0 +1,125 @@
+<template>
+  <b-container fluid id="browse">
+    <b-row class="position-fixed w-100">
+      <b-col cols="12" class="top-bar pt-3 ps-4 d-flex justify-content-between align-items-center">
+        <b-img src="https://fontmeme.com/permalink/210429/a3c965c8ca506ff7278f69241bb6a691.png" alt="Logo" width="100"
+               height="32.5">
+        </b-img>
+        <b-nav small class="flex-grow-1">
+          <b-nav-item v-for="(menu,index) in menus" :key="index">{{ menu.name }}</b-nav-item>
+        </b-nav>
+        <div class="icon d-flex justify-content-between align-items-center">
+          <i class="fas fa-search pe-3"></i>
+          <i class="far fa-bell"></i>
+          <b-dropdown size="lg" variant="link" toggle-class="text-decoration-none" no-caret>
+            <template #button-content>
+             <span>
+               <b-img width="32" height="32" :src="img_profile"></b-img>
+             </span>
+            </template>
+            <b-dropdown-item href="#">Menu</b-dropdown-item>
+          </b-dropdown>
+        </div>
+      </b-col>
+    </b-row>
+    <b-row>
+      <b-col cols="12 pe-0 ps-0 vh-100">
+        <div class="container-poster" v-if="jumboMovie.img !== ''">
+          <b-img :src="jumboMovie.img" fluid></b-img>
+          <div class="information-poster d-flex flex-column col-5">
+            <div class="title pb-3">
+              <h1>{{ jumboMovie.name }}</h1>
+            </div>
+            <div class="overview overflow-hidden col-9">
+              {{ jumboMovie.overview }}
+            </div>
+            <div class="button pt-5">
+              <b-button class="me-5">
+                <span class="pe-3"><i class="fas fa-play"></i></span><span>Riproduci</span>
+              </b-button>
+              <b-button>
+                <span class="pe-3"><i class="fas fa-info"></i></span><span>Altre info</span>
+              </b-button>
+            </div>
+          </div>
+        </div>
+      </b-col>
+    </b-row>
+  </b-container>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "Browse",
+  props: {
+    img_profile: String
+  },
+  data() {
+    return {
+      menus: [
+        {
+          name: 'Home',
+          url: '#'
+        }, {
+          name: 'Serie TV',
+          url: '#'
+        }, {
+          name: 'Film',
+          url: '#'
+        }, {
+          name: 'Nuovi e popolari',
+          url: '#'
+        }, {
+          name: 'La mia lista',
+          url: '#'
+        },
+      ],
+      APIData: {
+        APIKey: '8c6a3bb62200554563f12c8dd420d32f',
+        APILink: 'https://api.themoviedb.org/3',
+        posterLink: 'https://image.tmdb.org/t/p/w342'
+      },
+      trending: {
+        movie: [],
+        tv: []
+      },
+      jumboMovie: {
+        img: '',
+        name: '',
+        overview: ''
+      }
+    }
+  },
+  methods: {
+    async APIcall(url, name, variable) {
+      const axios_response = await axios.get(this.APIData.APILink + url,
+          {
+            params: {
+              api_key: this.APIData.APIKey,
+              language: 'it',
+              query: name
+            }
+          });
+      this[variable].movie = axios_response.data.results.filter(element => {
+        return element.media_type === 'movie';
+      });
+      this[variable].tv = axios_response.data.results.filter(element => {
+        return element.media_type === 'tv';
+      });
+
+    },
+    choosePoster() {
+      const movie = this.trending.movie[Math.floor(Math.random() * this.trending.movie.length)];
+      this.jumboMovie.img = 'https://image.tmdb.org/t/p/original' + movie.backdrop_path;
+      this.jumboMovie.name = movie.title;
+      this.jumboMovie.overview = movie.overview;
+    }
+  },
+  async created() {
+    await this.APIcall('/trending/all/day', '', 'trending');
+    this.choosePoster();
+  },
+}
+</script>
---
+++ src/views/Profile.vue
@@ -0,0 +1,52 @@
+<template>
+  <b-container fluid class="vh-100 overflow-hidden ps-0 pe-0" id="profile-choose">
+    <b-row>
+      <b-col cols="12" class="logo pt-3 ps-4">
+        <b-img src="https://fontmeme.com/permalink/210429/a3c965c8ca506ff7278f69241bb6a691.png" alt="Logo" width="100"
+               height="32.5">
+        </b-img>
+      </b-col>
+    </b-row>
+    <b-row class="h-100">
+      <b-col cols="12" class="d-flex flex-column justify-content-center align-items-center">
+        <div class="title-profile">
+          <h1>Chi vuole guardare Netflix?</h1>
+        </div>
+        <div class="list-profile pt-3">
+          <ul>
+            <li class="profile-user text-center overflow-hidden" v-for="(profile,index) in profiles" :key="index">
+              <router-link :to="{name: 'Browse', params:{img_profile : profile.picture.large}}">
+                <div class="profile-img-container overflow-hidden mb-3">
+                  <b-img :src="profile.picture.large" alt="profile" fluid></b-img>
+                </div>
+                <span class="name-profile">
+                  {{profile.name.first}}
+                </span>
+              </router-link>
+            </li>
+          </ul>
+        </div>
+      </b-col>
+    </b-row>
+  </b-container>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: "Profile",
+  data() {
+    return {
+      profiles: []
+    }
+  },
+  mounted() {
+    axios.get('https://randomuser.me/api/?inc=picture,name,login&results=4').then(response => {
+      this.profiles = response.data.results;
+    }).catch(error => {
+      console.log(error)
+    })
+  }
+}
+</script>
